2ccc86c3dc3157a3564bf97ba90705ed343585e8
a-2280 <khaliff.williamss@gmail.com>
2025-02-25 22:43:02 -0700

    Added the ability to display the todo list on the dom

--- script.js
+++ script.js
@@ -31,7 +31,53 @@ class TaskList {
     this.list.splice(task, 1);
   }
 }
+const taskList = new TaskList();
 
-const task1 = new Task("Test Title", "This is just a test");
-const task2 = new Task("Test Title 2", "This is just another test");
-const testList = new TaskList();
+document.addEventListener("DOMContentLoaded", function () {
+  const addTaskButton = document.querySelector(".add-task-button");
+  const dialog = document.querySelector(".dialog-box");
+  const formCancelButton = document.querySelector(".form-cancel-button");
+  const formSubmitButton = document.querySelector(".form-submit-button");
+  const title = document.querySelector("#title");
+  const description = document.querySelector("#description");
+  const form = document.querySelector(".form");
+  const errorElement = document.querySelector(".error-element");
+  const taskListContainer = document.querySelector(".task-list-container");
+
+  addTaskButton.addEventListener("click", () => {
+    errorElement.innerText = "";
+    title.value = "";
+    description.value = "";
+
+    dialog.showModal();
+  });
+
+  formCancelButton.addEventListener("click", () => {
+    dialog.close();
+  });
+
+  formSubmitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const messages = [];
+
+    if (title.value === "" || title.value === null) {
+      messages.push("Title is required");
+    }
+
+    if (messages.length > 0) {
+      errorElement.innerText = messages.join(", ");
+    }
+
+    const task = new Task(title.value, description.value);
+
+    taskList.addTaskToList(task);
+
+    const li = document.createElement("li");
+    li.innerText = `${task.title}${
+      task.description ? `: ${task.description}` : ""
+    }`;
+    taskListContainer.append(li);
+
+    dialog.close();
+  });
+});
